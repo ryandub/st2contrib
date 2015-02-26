d70bc8a18aa9d7c49006b4a566c5e343a2c0fd2e
Daniel Wallace <danielwallace@gtmanfred.com>
2015-02-26 13:46:43 -0600

    clean and fix update_tag

--- packs/github/actions/update_tag.py
+++ packs/github/actions/update_tag.py
@@ -1,4 +1,5 @@
 from lib.base import BaseGithubAction
+import re
 
 __all__ = [
     'UpdateTagAction'
@@ -6,14 +7,16 @@ __all__ = [
 
 
 class UpdateTagAction(BaseGithubAction):
-    def run(self, user, repo, tag, object='master'):
+    def run(self, user, repo, tag, object=None):
+        if object is None:
+            object = 'master'
 
         user = self._client.get_user(user)
         repo = user.get_repo(repo)
-        if not object.matches("[a-fA-F0-9]{40}"):
+        if not re.match(u"[a-fA-F0-9]{40}", object):
             branch = repo.get_branch(object)
             object = branch.commit.sha
-        tagobj = repo.get_git_ref('tags/%' % tag)
-        ref = tagobj.edit(object)
+        tagobj = repo.get_git_ref('tags/%s' % tag)
+        tagobj.edit(object)
 
-        return ref.object.sha
+        return tagobj.object.sha
